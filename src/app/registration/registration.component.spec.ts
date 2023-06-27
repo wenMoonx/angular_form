cd416f138c83735e51ca4af61ed517fff46e42df
wenMoonx <supertalented730@gmail.com>
2023-06-27 05:32:24 -0400

    fix: added readme and test helper

--- src/app/registration/registration.component.spec.ts
+++ src/app/registration/registration.component.spec.ts
@@ -1,21 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrationComponent } from './registration.component';
+import { ApiService } from '../services/api.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  dispatchFakeEvent,
+  setFieldValue,
+} from '../spec-helpers/element.spec-helpers';
+import {
+  email,
+  firstName,
+  lastName,
+  password,
+} from '../spec-helpers/signup-data.spec-helpers';
+import { DebugElement } from '@angular/core';
+
+const requiredFields = [
+  'firstName',
+  'lastName',
+  'email',
+  'password',
+  'confirmPassword',
+];
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
   let fixture: ComponentFixture<RegistrationComponent>;
+  let apiService: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RegistrationComponent]
+      imports: [ReactiveFormsModule],
+      declarations: [RegistrationComponent],
+      providers: [{ provide: apiService, useValue: apiService }],
     });
     fixture = TestBed.createComponent(RegistrationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  const fillForm = () => {
+    setFieldValue(fixture, 'firstName', firstName);
+    setFieldValue(fixture, 'lastName', lastName);
+    setFieldValue(fixture, 'email', email);
+    setFieldValue(fixture, 'password', password);
+    setFieldValue(fixture, 'confirmPassword', password);
+  };
+
+  const markFieldAsTouched = (element: DebugElement) => {
+    dispatchFakeEvent(element.nativeElement, 'blur');
+  };
+
+  it('submits the form successfully', () => {
     expect(component).toBeTruthy();
   });
 });
---
+++ src/app/spec-helpers/signup-data.spec-helpers.ts
@@ -0,0 +1,4 @@
+export const firstName = 'Mykyta';
+export const lastName = 'Suzuki';
+export const email = 'mykyta1007@gamil.com';
+export const password = 'Mykyta1007!';
